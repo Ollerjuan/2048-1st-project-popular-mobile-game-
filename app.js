bb90d8e63f598442e14367cdf7c318bab2b6bd29
Juan <juan_oller@yahoo.com>
2022-04-06 15:04:02 -0500

    created my pseudo-code

---
+++ app.js
@@ -0,0 +1,45 @@
+// I will first pull all the values that i will be using from the html 
+
+
+
+
+
+
+
+// create a function for the start button to allow the user to begin playing 
+
+
+
+
+
+// create my first array with the frst two numbers that can be used to start and countiue the game (2 & 4)
+
+
+
+
+
+
+
+
+
+
+
+// create my function that will allow the numbers to double when they are swipped into each other but only if they are the same number 
+
+
+
+
+
+//create a function that will allow a 2 or a 4 to pop up every time a number is swipped into an empty grid
+
+
+
+
+// create win game once the player has reach 2048 
+
+
+
+
+
+
+// create an end game if the player does not have anymore space left to swipe and has not reached 2048
